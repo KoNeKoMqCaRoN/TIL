93fae1109070368035649a2f890b98138deab5f2
KoNeKoMqCaRoN <shoukun733@gmail.com>
2025-10-12 02:44:16 +0900

    Update MySQL.sql

---
+++ MySQL.sql
@@ -0,0 +1,25 @@
+-- 2025/09/26 mysqlサーバースタート、ストップ
+mysql.server start
+mysql.server stop
+mysql.server restart
+
+
+-- 2025/10/03 ログイン、DESCと射影、SELECT文、算術演算、列見出し
+mysql -u root -p
+mysql -u cm -p studyDB
+
+DESC pokemon_zukann;
+SELECT *
+FROM pokemon_zukann;
+
+SELECT zukann_no, pokemon_name, evolution_seq
+FROM pokemon_zukann;
+
+SELECT gname, price, price * 1.2
+FROM goods;
+
+SELECT gname, price, price * 1.2 as new_price
+FROM goods;
+
+SELECT ename, sal, comm, (sal + comm) * 2 as summer_bonus
+FROM emp;
